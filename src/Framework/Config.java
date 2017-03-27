ed33416e6bc4ee41049b837b05c24713e908feab
Peter Schriever <peterzen72@gmail.com>
2017-03-27 18:59:27 +0200

    added Config class, which reads from .properties files in the configs/ directory

--- src/Framework/Config.java
+++ src/Framework/Config.java
@@ -1,8 +1,36 @@
 package Framework;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Properties;
+
 /**
  * Created by peterzen on 2017-03-27.
  * Part of the framework project.
  */
-public class Config {
+public final class Config {
+    private static HashMap<String, Properties> props = new HashMap<>();
+
+    private Config() {
+    }
+
+    private static void readProperties(String configFile) throws IOException {
+        String filePath = "configs/"+configFile+".properties";
+        InputStream inputStream;
+        Properties newProps = new Properties();
+
+        inputStream = Config.class.getClassLoader().getResourceAsStream(filePath);
+        newProps.load(inputStream);
+        props.put(configFile, newProps);
+    }
+
+    public static Object get(String configFile, String key) throws IOException {
+        if (props.containsKey(configFile)) {
+            return props.get(configFile).get(key);
+        }
+        readProperties(configFile);
+        return get(configFile, key);
+    }
+
 }
--- src/Framework/Start.java
+++ src/Framework/Start.java
@@ -1,13 +1,17 @@
 package Framework;
 
+import java.io.IOException;
+
 /**
  * Created by peterzen on 2017-03-27.
  * Part of the framework project.
  */
 public class Start {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World from Framework.Start");
+
+        System.out.println(Config.get("network", "ip"));
     }
 
 }
