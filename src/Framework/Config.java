359c5cf0c43b4b95d7c34aecd88d70a911289d02
Peter Schriever <peterzen72@gmail.com>
2017-03-27 13:44:50 +0200

    structuur uitgewerkt

---
+++ src/Framework/Config.java
@@ -0,0 +1,8 @@
+package Framework;
+
+/**
+ * Created by peterzen on 2017-03-27.
+ * Part of the framework project.
+ */
+public class Config {
+}
---
+++ src/Framework/Start.java
@@ -0,0 +1,13 @@
+package Framework;
+
+/**
+ * Created by peterzen on 2017-03-27.
+ * Part of the framework project.
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from Framework.Start");
+    }
+
+}
