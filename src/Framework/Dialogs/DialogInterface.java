bb0b31fbfc4cddcce4003c90c54de1c91168bca6
Ruben <rbuisman95@gmail.com>
2017-03-30 15:01:15 +0200

    Added interface

---
+++ src/Framework/Dialogs/DialogInterface.java
@@ -0,0 +1,16 @@
+package Framework.Dialogs;
+
+/**
+ * Class UserNameDialog
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (30-03-2017)
+ */
+public interface DialogInterface {
+
+    void display();
+
+    void setupDialog();
+
+    void executeCallback();
+}
