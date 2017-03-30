cf54a02cda00afda9cd81977c14a6b13a34af3cc
Ruben <rbuisman95@gmail.com>
2017-03-30 15:25:06 +0200

    Created ErrorDialog

---
+++ src/Framework/Dialogs/ErrorDialog.java
@@ -0,0 +1,22 @@
+package Framework.Dialogs;
+
+/**
+ * Class ErrorDialog
+ *
+ * @author Ruben Buisman
+ * @version 0.1 (30-03-2017)
+ */
+public class ErrorDialog {
+
+    public void display(){
+
+    }
+
+    public void setupDialog(){
+
+    }
+
+    public void executeCallback(){
+
+    }
+}
