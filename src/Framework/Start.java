45e64e590e1d3fd0e8d00a44df1bef7aabe1dcec
Peter Schriever <peterzen72@gmail.com>
2017-04-06 13:07:19 +0200

    commented fw networking tests and code clean up

--- src/Framework/Start.java
+++ src/Framework/Start.java
@@ -1,22 +1,14 @@
 package Framework;
 
 import Framework.GUI.BaseController;
-import Framework.Networking.Connection;
-import Framework.Networking.NetworkEvents;
-import Framework.Networking.Request.GetGameListRequest;
-import Framework.Networking.Request.GetPlayerListRequest;
-import Framework.Networking.Request.LoginRequest;
-import Framework.Networking.Request.Request;
-import Framework.Networking.Response.Response;
-import Framework.TempTTTGame.TTTGameStart;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -28,7 +20,6 @@ public class Start extends Application {
     private Stage stage = null;
     private Scene scene = null;
     private String[] args;
-    private TTTGameStart runningGame = null;
 
     public Start() {
     }
@@ -40,82 +31,82 @@ public class Start extends Application {
     }
 
     public static void main(String[] args) throws Exception {
-        String host = Config.get("network", "host");
-        int port = Integer.parseInt(Config.get("network", "port"));
+//        String host = Config.get("network", "host");
+//        int port = Integer.parseInt(Config.get("network", "port"));
 
         // setup receiver
-        Connection conn = new Connection(host, port, new NetworkEvents() {
-            @Override
-            public void challengeCancelled(Response response) {
-                System.out.println("challengeCancelled event called!");
-            }
-
-            @Override
-            public void challengeReceived(Response response) {
-                System.out.println("challengeReceived event called!");
-            }
-
-            @Override
-            public void ErrorReceived(Response response) {
-                System.out.println("ErrorReceived event called!");
-            }
-
-            @Override
-            public void gameEnded(Response response) {
-                System.out.println("gameEnded event called!");
-            }
-
-            @Override
-            public void gameListReceived(Response response) {
-                System.out.println("gameListReceived event called!");
-            }
-
-            @Override
-            public void matchReceived(Response response) {
-                System.out.println("matchReceived event called!");
-            }
-
-            @Override
-            public void moveReceived(Response response) {
-                System.out.println("moveReceived event called!");
-            }
-
-            @Override
-            public void ourTurn(Response response) {
-                System.out.println("ourTurn event called!");
-            }
-
-            @Override
-            public void playerListReceived(Response response) {
-                System.out.println("playerListReceived event called!");
-            }
-
-            @Override
-            public void errorReceived(Response response) {
-                System.out.println("errorReceived event called!");
-            }
-        });
+//        Connection conn = new Connection(host, port, new NetworkEvents() {
+//            @Override
+//            public void challengeCancelled(Response response) {
+//                System.out.println("challengeCancelled event called!");
+//            }
+//
+//            @Override
+//            public void challengeReceived(Response response) {
+//                System.out.println("challengeReceived event called!");
+//            }
+//
+//            @Override
+//            public void ErrorReceived(Response response) {
+//                System.out.println("ErrorReceived event called!");
+//            }
+//
+//            @Override
+//            public void gameEnded(Response response) {
+//                System.out.println("gameEnded event called!");
+//            }
+//
+//            @Override
+//            public void gameListReceived(Response response) {
+//                System.out.println("gameListReceived event called!");
+//            }
+//
+//            @Override
+//            public void matchReceived(Response response) {
+//                System.out.println("matchReceived event called!");
+//            }
+//
+//            @Override
+//            public void moveReceived(Response response) {
+//                System.out.println("moveReceived event called!");
+//            }
+//
+//            @Override
+//            public void ourTurn(Response response) {
+//                System.out.println("ourTurn event called!");
+//            }
+//
+//            @Override
+//            public void playerListReceived(Response response) {
+//                System.out.println("playerListReceived event called!");
+//            }
+//
+//            @Override
+//            public void errorReceived(Response response) {
+//                System.out.println("errorReceived event called!");
+//            }
+//        });
 
         // setup Response observer
-        conn.setupInputObserver();
+//        conn.setupInputObserver();
 
         // concrete command(s) and their invocation(s):
 
         // test login command
-        Request loginRequest = new LoginRequest(conn, "player");
-        loginRequest.execute();
+//        Request loginRequest = new LoginRequest(conn, "player");
+//        loginRequest.execute();
 
         // test logout command (destroys connection)
 //        Request logoutRequest = new LogoutRequest(conn);
 //        logoutRequest.execute();
 
         // test get gamelist command
-        Request getGameListRequest = new GetGameListRequest(conn);
-        getGameListRequest.execute();
+//        Request getGameListRequest = new GetGameListRequest(conn);
+//        getGameListRequest.execute();
 
         // test get playerlist command
-        Request getPlayerListRequest = new GetPlayerListRequest(conn);
-        getPlayerListRequest.execute();
+//        Request getPlayerListRequest = new GetPlayerListRequest(conn);
+//        getPlayerListRequest.execute();
 
         // test subscribe command
 //        Request subscribeRequest = new SubscribeRequest(conn, "Tic-tac-toe");
@@ -182,15 +173,7 @@ public class Start extends Application {
         return scene;
     }
 
-    public void setRunningGame(TTTGameStart runningGame) {
-        this.runningGame = runningGame;
-    }
-
-    public TTTGameStart getRunningGame() {
-        return runningGame;
-    }
-
-    public void loadGameModule(String path) throws MalformedURLException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
+    public void loadGameModule(String path) throws IOException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
         URL url = new URL(path);
         JarClassLoader loader = new JarClassLoader(url);
         loader.invokeClass("Start", new String[]{"0"}, getStage(), getScene());
